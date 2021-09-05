023bb95e04d0e5dbfe2c1c99259ea8d4b7996ede
Qabas al ani <doctorqa@yahoo.com>
2021-09-05 14:03:26 -0400

    added the user-seeds.js to create an araay of objects for the users

---
+++ seeds/user-seeds.js
@@ -0,0 +1,17 @@
+const { User } = require('../models');
+
+const userData = [{
+        username: 'Xandromus',
+        password: '12345678'
+
+    },
+    {
+        username: 'Lernantino',
+        password: '01234567'
+    },
+  
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
